e41363cb940b5caa664894fe3dd52ddef57f4439
Piotr Wlodek <piotr.wlodek@avanade.com>
2018-01-16 14:06:54 +0100

    added last shipping microservice

---
+++ Services/ECommerce.Shipping.Host/Host.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+using Autofac;
+using ECommerce.Shipping.Host.Modules;
+using MassTransit;
+
+namespace ECommerce.Shipping.Host
+{
+    public class Host
+    {
+        public Host()
+        {
+        }
+
+        public void Run()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterModule<BusModule>();
+            //builder.RegisterModule<ConsumerModule>();
+
+            var container = builder.Build();
+            var bus = container.Resolve<IBusControl>();
+            bus.Start();
+
+            Thread.Sleep(int.MaxValue);
+        }
+    }
+}
---
+++ Services/ECommerce.Shipping.Host/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ECommerce.Shipping.Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
